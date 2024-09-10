61db5e34a18bf8c8f690589dcee5adba9f8a2f72
JanithRavinduRashmika <rashmikajanithravindu@gmail.com>
2024-09-11 03:33:08 +0530

    Update Strata.R

--- JCausalInf/R/Strata.R
+++ JCausalInf/R/Strata.R
@@ -1,4 +1,8 @@
 Strata <- function() {
-  # This is starata
+  paste("Use the dataset homocyst in the package senstrat. The outcome is homocysteine, the homocysteine level, and the treatment is z, where z = 1 for a daily smoker and z = 0 for a never smoker.
+Covariates are female, age3, ed3, bmi3, pov2 with detailed explanations in the package, and
+st is a stratum indicator, defined by all the combinations of the discrete covariates.")
+  paste("a) How many strata have only treated or control units?")
+  paste("b) What is the proportion of the units in these strata?")
 }
 
